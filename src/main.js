2a1801edc034836b93a232925ce4db62be19d1c0
Alvaro Cabrera <pateketrueke@gmail.com>
2017-12-08 15:48:19 -0600

    Skip prompt() when data-whatsapp-prompt is missing; fix

---
+++ src/main.js
@@ -0,0 +1,127 @@
+'use strict';
+
+const defaults = {
+  triggerIf: /android|ios|ipad/i,
+  uaString: window.navigator.userAgent,
+  mode: 'append',
+  newNodeSelector: 'a',
+  whatsappApiProtocol: 'whatsapp://send',
+  whatsappApiUrl: 'https://api.whatsapp.com/api/send',
+  promptMessage: 'What do you want to say?',
+  elementCallback: null,
+};
+
+function checkIfSupported(config) {
+  if (config.triggerIf instanceof RegExp) {
+    return config.triggerIf.test(config.uaString);
+  }
+
+  if (typeof config.triggerIf === 'function') {
+    return config.triggerIf();
+  }
+
+  return !!config.triggerIf;
+}
+
+function assign(target) {
+  for (let i = 1; i < arguments.length; i += 1) {
+    if (arguments[i]) {
+      Object.keys(arguments[i]).forEach(key => {
+        target[key] = arguments[i][key];
+      });
+    }
+  }
+
+  return target;
+}
+
+function setupLink(node, newEl, config, isSupported) {
+  let fixedLink = isSupported
+    ? config.whatsappApiProtocol
+    : config.whatsappApiUrl;
+
+  if (node.dataset.whatsapp) {
+    fixedLink += `?phone=${node.dataset.whatsapp}`;
+  }
+
+  if (newEl.tagName === 'A') {
+    newEl.href = fixedLink;
+  }
+
+  newEl.addEventListener('click', e => {
+    if (!isSupported) {
+      e.preventDefault();
+    }
+
+    const msg = (typeof node.dataset.whatsappPrompt !== 'undefined'
+      ? prompt(node.dataset.whatsappPrompt || config.promptMessage) // eslint-disable-line no-alert
+      : null) || node.dataset.whatsappMessage;
+
+    const url = msg
+      ? `${fixedLink}${fixedLink.indexOf('?') === -1 ? '?' : '&'}text=${msg}`
+      : fixedLink;
+
+    if (newEl.tagName === 'A') {
+      newEl.href = url;
+    }
+
+    if (!isSupported) {
+      window.open(url);
+    }
+  });
+}
+
+function makeEl(config) {
+  // TODO: allow a#foo.bar syntax as shortcut
+  const target = document.createElement(config.newNodeSelector);
+
+  return target;
+}
+
+function replace(node, config, isSupported) {
+  const newEl = makeEl(config);
+  const body = node.innerHTML;
+
+  node.parentNode.insertBefore(newEl, node);
+  node.parentNode.removeChild(node);
+
+  newEl.innerHTML = body;
+
+  setupLink(node, newEl, config, isSupported);
+}
+
+function append(node, config, isSupported) {
+  const newEl = makeEl(config);
+  const body = node.innerHTML;
+
+  node.innerHTML = '';
+  node.appendChild(newEl);
+
+  newEl.innerHTML = body;
+
+  setupLink(node, newEl, config, isSupported);
+}
+
+function init(options) {
+  const config = assign({}, defaults, options);
+  const isSupported = checkIfSupported(config);
+  const matchedElements = document.querySelectorAll('[data-whatsapp]');
+
+  for (let i = 0; i < matchedElements.length; i += 1) {
+    if (typeof config.elementCallback === 'function') {
+      config.elementCallback(matchedElements[i]);
+    }
+
+    if (config.mode === 'append') {
+      append(matchedElements[i], config, isSupported);
+    } else if (config.mode === 'replace') {
+      replace(matchedElements[i], config, isSupported);
+    } else {
+      throw new Error(`Unsupported mode '${config.mode}'`);
+    }
+  }
+}
+
+export default {
+  init,
+};
